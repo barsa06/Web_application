c8e1bfd7ec2a4050fbae16747f091ff86510b23a
unknown <bpun7891@gmail.com>
2026-02-04 15:34:07 +0545

    folder structure

--- Web_application/vite-project/src/components/register/Register.jsx
+++ Web_application/vite-project/src/components/register/Register.jsx
@@ -1,17 +1,19 @@
 function Register(){
   return(
-    <div>
-        <h1>
-         login form
-        </h1>
-           <label for="username">Username:</label>
-           <input type="text" id="username" name="username"></input>
+    <div class="italic ...">
+      <div>
+        <h2 class ="text-2xl font-semibold" >
+         Login Page</h2>
+           <div class ="rounded-xl"><label for="username">Username:</label>
+           <input type="text" id="username" name="username" class ="rounded-xl"></input><br/>
 
            <label for="password">Password:</label>
-           <input type="password"></input>
+           <input type="password"></input><br/>
 
            <label for="email">Email:</label>
-           <input type="email" id="email" name="pwd"></input>
+           <input type="email" id="email" name="pwd"></input><br/>
+           </div>
+    </div>
     </div>
   )
 };
