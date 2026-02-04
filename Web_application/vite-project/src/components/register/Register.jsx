426154dc32264d036558e78f19039c5cc35d9c5c
barsa06 <bpun7891@gmail.com>
2026-02-04 08:41:49 +0545

    css in login page

--- Web_application/vite-project/src/components/register/Register.jsx
+++ Web_application/vite-project/src/components/register/Register.jsx
@@ -1,17 +1,22 @@
 function Register(){
   return(
-    <div>
-        <h1>
-         login form
+    <div className="italic w-full max-w-md  m-10 p-20 border-2 border-black rounded-xl bg-blue-200">
+        <h1 className="italic text-3xl font-bold" align="left">
+         **Login form**
         </h1>
-           <label for="username">Username:</label>
-           <input type="text" id="username" name="username"></input>
+        <form className="mt-15">
+           <label for="username" className="mt-3 block text-sm font-medium text-gray-700">Username:</label>
+           <input type="text" id="username" name="username" className=" border-2 rounded-lg p-2"></input><br/>
 
-           <label for="password">Password:</label>
-           <input type="password"></input>
+           <label for="password" className="mt-3 block text-sm font-medium text-gray-700">Password:</label>
+           <input type="password" id="password" name="password" className="border-2 rounded-lg p-2"></input><br/>
 
-           <label for="email">Email:</label>
-           <input type="email" id="email" name="pwd"></input>
+           <label for="email" className="mt-3 block text-sm font-medium text-gray-700">Email:</label>
+           <input type="email" id="email" name="email" className="border-2 rounded-lg p-2"></input><br/>
+
+           <input type="Submit" value="Log in" id="Login" className="mt-3 block text-sm font-medium text-gray-700
+           border-1 border-black rounded-md p-2 bg-green-500"/>
+           </form>
     </div>
   )
 };
